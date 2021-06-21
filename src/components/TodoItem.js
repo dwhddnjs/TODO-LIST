326ab1420247ee967c83a52d568ac0931aa84054
이종원 <per1215@naver.com>
2021-06-21 23:10:06 +0900

    add TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import { MdDone, MdDelete } from "react-icons/md";
+
+const TodoItemBlock = styled.div`
+`
+const CheckCircle = styled.div`
+`
+
+export default function TodoItem() {
+  return <div></div>;
+}
