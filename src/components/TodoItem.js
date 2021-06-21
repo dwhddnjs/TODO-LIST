3933d3778aaaefd1f7716fb808a07ca87d78df26
이종원 <per1215@naver.com>
2021-06-22 01:24:28 +0900

    finished ui

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,35 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
+
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #e8d9a9;
+      color: #38d9a9;
+    `}
+`;
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
 
 const Remove = styled.div`
   opacity: 0;
@@ -15,16 +44,27 @@ const Remove = styled.div`
   }
 `;
 
-const CheckCircle = styled.div``;
-const Text = styled.div``;
 const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+
   &:hover {
-    $(Remove) {
+    ${Remove} {
       opacity: 1;
     }
   }
 `;
 
-export default function TodoItem() {
-  return <div></div>;
+export default function TodoItem({ id, done, text }) {
+  return (
+    <TodoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove>
+        <MdDelete />
+      </Remove>
+    </TodoItemBlock>
+  );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -9,5 +10,12 @@ const TodoListBlock = styled.div`
 `;
 
 export default function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성" done={true} />
+      <TodoItem text="컴포넌트 스타일링" done={true} />
+      <TodoItem text="Context 만들기" done={false} />
+      <TodoItem text="기능 구현" done={false} />
+    </TodoListBlock>
+  );
 }
