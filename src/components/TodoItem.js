e43c98337eb90e77ce14a786d3a650e2ebabe25a
이종원 <per1215@naver.com>
2021-06-21 23:14:31 +0900

    waiting

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
 
+const Remove = styled.div`
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+`;
+
+const CheckCircle = styled.div``;
+const Text = styled.div``;
 const TodoItemBlock = styled.div`
-`
-const CheckCircle = styled.div`
-`
+  &:hover {
+    $(Remove) {
+      opacity: 1;
+    }
+  }
+`;
 
 export default function TodoItem() {
   return <div></div>;
